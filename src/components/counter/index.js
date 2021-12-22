6323fa757986d1e2c1688ac689f475bb53a57a6b
David Castellanos <davcastellanoslarios@gmail.com>
2021-12-22 12:02:58 -0600

    adding actions

--- src/components/counter/index.js
+++ src/components/counter/index.js
@@ -1,9 +1,11 @@
+import {deposit, withdraw} from '../../store/amount/action'
+ 
 const Counter = () =>{
     return(
     <div>
         <h1>{`Amount`}</h1>
-        <button>Deposit 10</button>
-        <button>Withdraw 10</button>
+        <button onClick={() => deposit}>Deposit 10</button>
+        <button onClick={() => withdraw }> Withdraw 10</button>
     </div>  
     )
 }
