131dd792ebdf57752cbd25495055f50a4606d63a
David Castellanos <davcastellanoslarios@gmail.com>
2021-12-22 11:27:56 -0600

    importing counter index to the app file

---
+++ src/components/counter/index.js
@@ -0,0 +1,11 @@
+const Counter = () =>{
+    return(
+    <div>
+        <h1>{`Amount`}</h1>
+        <button>Deposit 10</button>
+        <button>Withdraw 10</button>
+    </div>  
+    )
+}
+
+export default Counter;
